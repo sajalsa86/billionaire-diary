78fc2b199c7c88d1f63256ebe27aaa713db1b0a2
sajalsa86e <sajalsm86@gmail.com>
2023-09-22 21:58:14 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const styleHelloWorld = document.getElementById('hello');
+styleHelloWorld.style.color = 'azure';
+styleHelloWorld.style.backgroundColor = 'green';
+styleHelloWorld.style.padding = '10px';
+styleHelloWorld.style.width = '25%';
